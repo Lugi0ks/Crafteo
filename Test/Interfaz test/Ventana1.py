40246dbf29bb937122746c8817cdc136890f6872
Lugi0ks <juliodz45@gmail.com>
2021-10-28 14:40:41 -0300

    opa

--- Test/Interfaz test/Ventana1.py
+++ Test/Interfaz test/Ventana1.py
@@ -6,13 +6,23 @@ class Ventana():
 
     #Cuerpo de la ventana
     def __init__(self):
+        #Configuracion de la ventana
         self.ventana1 = tk.Tk()
+        self.ventana1.title('Calculadora de Crafteo')
+        #self.ventana1.config(bg="dark turquoise")
+        self.ventana1.iconbitmap('D:\Python\Craft2\Test\Interfaz test\icons.ico')
+
+        #Frame 1 y su contenido
         self.lf1 = LabelFrame(self.ventana1, text = 'Seleccion de Materiales')
         self.lf1.grid(column=0, row=0, padx=5, pady=10)
         self.Material()
+
+        #Frame 2 y su contenido
         self.lf2 = LabelFrame(self.ventana1, text = 'Primeros datos')
         self.lf2.grid(column=0, row=1, padx=5, pady=10)
         self.Ppasos()
+
+        #loop de la ventana
         self.ventana1.mainloop()
 
     #Contenido del primer LabelFrame
