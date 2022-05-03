21d31ee2040f68f62d392e0728cb309a8f2aa756
Lugi0ks <juliodz45@gmail.com>
2022-05-02 23:36:08 -0400

    ..

--- Test/Interfaz test/Nueva Ventana.py
+++ Test/Interfaz test/Nueva Ventana.py
@@ -1,13 +1,18 @@
 import tkinter as tk
 
+
+# Segunda Ventana
 def createNewWindow():
     newWindow = tk.Toplevel(app)
     labelExample = tk.Label(newWindow, text = "New Window")
     buttonExample = tk.Button(newWindow, text = "New Window button")
 
     labelExample.pack()
+
     buttonExample.pack()
 
+
+# Primera ventana
 app = tk.Tk()
 buttonExample = tk.Button(app, 
               text="Create new window",
